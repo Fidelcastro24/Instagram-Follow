5ee1dfbab928f29b763a2f8933ca3d4e673a2ec4
Fidel Castro <74963732+Fidelcastro24@users.noreply.github.com>
2022-11-16 20:14:28 +0300

    Update insfollow.sh

--- insfollow.sh
+++ insfollow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# insfollow v2.0
-# recoded by: Termux Professor
-# github.com/termuxprofessor/insfollow
+# instagramfollow v2.0
+# recoded by: Fidel Castro 
+# https://github.com/Fidelcastro24/Instagram-Follow
 # If you use any part from this code, give me the credits, please, read the License
 
 clear
